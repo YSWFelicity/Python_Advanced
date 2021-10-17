f243c0bad45187631e9bc187dc3c410a43f7d649
YSW <wang.yings@northeastern.edu>
2021-10-17 16:36:33 -0700

    hw05 completed

---
+++ hw05/upc.py
@@ -0,0 +1,41 @@
+'''
+Yingshu Wang
+CS5001 Fall 2021
+
+A program that test if a UPC has been entered incorrectly during checkout.
+'''
+
+
+def is_valid_UPC(str_UPC):
+    '''
+        Function -- is_valid_UPC
+            Checks if a given string is a valid UPC.
+        Parameter:
+            str_UPC -- The string to check
+        Returns:
+            True if a given string is a multiple of 10, False otherwise.
+    '''
+    total = 0
+    # iterate from left to right in UPC
+    for i, char in enumerate(reversed(str_UPC)):
+        if i % 2 == 0:
+            # Add numbers in even positions to toal
+            total += int(char)
+        else:
+            # Multiply numbers in odd positions by 3 then add to total
+            total += 3*int(char)
+
+    # Test if total is a multiple of 10
+    return total % 10 == 0
+
+
+def main():
+    upc = input("Enter a UPC number:\n")
+    if is_valid_UPC(upc):
+        print("Valid UPC!")
+    else:
+        print("Not a valid UPC.")
+
+
+if __name__ == "__main__":
+    main()
