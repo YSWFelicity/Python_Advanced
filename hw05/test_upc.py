699f46ceaae3f9b307ab1bccf250c2196bc92e78
YSW <wang.yings@northeastern.edu>
2021-10-17 16:39:23 -0700

    hw05 completed

---
+++ hw05/test_upc.py
@@ -0,0 +1,16 @@
+'''
+Yingshu Wang
+CS5001 Fall 2021
+
+Test upc.py
+'''
+
+
+from upc import is_valid_UPC
+
+
+def test_is_valid_UPC():
+    assert(is_valid_UPC("9780128053904") is True)
+    assert(is_valid_UPC("75839523467") is False)
+    assert(is_valid_UPC("796030114977") is True)
+    assert(is_valid_UPC("657893408568") is False)
