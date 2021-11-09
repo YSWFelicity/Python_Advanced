c44e3f464e0f3a9db757e7a9754ce465f747cda9
YSW <wang.yings@northeastern.edu>
2021-11-08 17:23:01 -0800

    hw06 completed

---
+++ hw06/vowelsearch.py
@@ -0,0 +1,52 @@
+'''
+Yingshu Wang
+CS5001 Fall 2021
+
+A program that get basic directions on the Seattle Link light rail.
+'''
+
+
+def voewel_search_in_word(word):
+    '''
+    Function -- voewel_search_in_word
+        Check if there is a voewel in a string.
+    Parameters:
+        word -- A string
+    Returns:
+        True if a word contains a vowel. Otherwise, false.
+    '''
+    if len(word) == 0:
+        return False
+    elif len(word) == 1:
+        if word.lower() in "aeiou":
+            return True
+        else:
+            return False
+    else:
+        if voewel_search_in_word(word[0]):
+            return True
+        else:
+            return voewel_search_in_word(word[1:])
+
+
+def contains_vowel(lst_str):
+    '''
+    Function -- contains_vowel
+        Check if there is a voewel in every string.
+    Parameters:
+        lst_str -- A list of strings
+    Returns:
+        True if every string in the list contains a vowel.Otherwise, false.
+    '''
+    if len(lst_str) == 0:
+        return False
+    elif len(lst_str) == 1:
+        if voewel_search_in_word(lst_str[0]):
+            return True
+        else:
+            return False
+    else:
+        if voewel_search_in_word(lst_str[0]):
+            return contains_vowel(lst_str[1:])
+        else:
+            return False
