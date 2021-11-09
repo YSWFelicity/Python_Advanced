14811637c9d191d8f68ea7086b9944e47bfa5a7e
YSW <wang.yings@northeastern.edu>
2021-11-08 17:24:00 -0800

    hw06 completed

---
+++ hw06/test_vowelsearch.py
@@ -0,0 +1,24 @@
+'''
+Yingshu Wang
+CS5001 Fall 2021
+
+A program that get basic directions on the Seattle Link light rail.
+'''
+
+
+from vowelsearch import voewel_search_in_word, contains_vowel
+
+
+def test_voewel_search_in_word():
+    assert(voewel_search_in_word("garage"))
+    assert(voewel_search_in_word("this"))
+    assert(not voewel_search_in_word("fffff"))
+    assert(not voewel_search_in_word(""))
+
+
+def test_contains_vowel():
+    assert(contains_vowel(["garage", "this", "man"]))
+    assert(not contains_vowel(["ffff", "this", "man"]))
+    assert(not contains_vowel([]))
+    assert(not contains_vowel([""]))
+    assert(not contains_vowel(["", ""]))
